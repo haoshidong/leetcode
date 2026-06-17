a16175e08508708777ad9771f5ffc722c2aa2bcd
shidonghao <shidonghao@tencent.com>
2026-06-17 19:38:48 +0800

    chore: 新增 SQLite 数据源配置并开启提交信息 issue 键校验

--- code/src/new_hot100/P20_ValidParentheses.java
+++ code/src/new_hot100/P20_ValidParentheses.java
@@ -1,25 +1,31 @@
 package new_hot100;
 
+import java.util.HashMap;
+import java.util.Stack;
+
 class P20_ValidParentheses {
 
     
 //IMPORTANT!! Submit Code Region Begin(Do not remove this line)
 class Solution {
     public boolean isValid(String s) {
-        int o = 0;
-        int p = 0;
-        int q = 0;
-        for(char c : s.toCharArray()) {
-            switch (c){
-                case '(' : o++; break;
-                case ')' : o--; if(o < 0) return false; break;
-                case '[' : p++; break;
-                case ']' : p--; if(p < 0) return false; break;
-                case '{' : q++; break;
-                case '}' : q--; if(q < 0) return false; break;
+        Stack<Character> stack = new Stack<>();
+        HashMap<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+        for (char c : s.toCharArray()) {
+            if(map.containsKey(c)){
+                if(!stack.isEmpty() && stack.peek() == map.get(c)){
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else {
+                stack.push(c);
             }
         }
-        return o == 0 && p == 0 && q == 0;
+        return stack.size() == 0;
     }
 }
 //IMPORTANT!! Submit Code Region End(Do not remove this line)
