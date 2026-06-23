d4445f40c1ce14e8e781577c9dd76e1831e0c56d
ryan <1240743991@qq.com>
2026-06-23 22:26:42 +0800

    feat

--- code/src/new_hot100/P55_JumpGame.java
+++ code/src/new_hot100/P55_JumpGame.java
@@ -1,25 +1,29 @@
 package new_hot100;
 
-class P53_MaximumSubarray {
+class P55_JumpGame {
 
     
 //IMPORTANT!! Submit Code Region Begin(Do not remove this line)
 class Solution {
-    public int maxSubArray(int[] nums) {
+    public boolean canJump(int[] nums) {
         int[] dp = new int[nums.length];
-        dp[0] = nums[0];
-        int max = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            dp[i] = dp[i - 1] > 0 ? dp[i-1] + nums[i] : nums[i];
-            max = Math.max(max, dp[i]);
+        if(nums[0] == 0 && nums.length > 1) {
+            return false;
         }
-        return max;
+        dp[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if(dp[i-1] - 1 < 0){
+                return false;
+            }
+            dp[i] = Math.max(dp[i - 1] - 1, nums[i]);
+        }
+        return true;
     }
 }
 //IMPORTANT!! Submit Code Region End(Do not remove this line)
 
     public static void main(String[] args) {
         // add your test code
-        Solution solution = new P53_MaximumSubarray().new Solution(); 
+        Solution solution = new P55_JumpGame().new Solution(); 
     }
 }
